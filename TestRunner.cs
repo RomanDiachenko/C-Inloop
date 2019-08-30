99eaf416a2c2f50b87ee761aa3babb486b8b363e
RomanDiachenko <52920901+RomanDiachenko@users.noreply.github.com>
2019-08-30 09:37:52 +0300

    Decomposition and comments code

---
+++ TestRunner.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Text;
+using System.Threading;
+using Tes
+
+namespace TestRunner
+{ 
+    public class Runner
+    { 
+        {
+        private IWebDriver _driver;
+        privare TestLogin _testlogin
+
+        [Test]
+        {
+
